f8ab5f6d22eb6cf3b567eea07064a11e453a5890
Sushanth Bobby Lloyds <bobby.dreamer@gmail.com>
2022-08-06 19:59:42 +0530

    Testing Middleware

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const app = express();
 
+
+/************************************************/
+// Middleware
+
+// Simple request time logger
+app.use((req, res, next) => {
+    console.log("A new request received at " + Date.now());
+ 
+    // This function call tells that more processing is
+    // required for the current request and is in the next middleware
+    
+    // function/route handler.
+    next();  
+});
+
+app.use((req, res, next) => {
+    console.log("Just another middleware : " + Date.now());
+    next();  
+});
+
+//Simple request time logger for a specific route
+app.use('/about', (req, res, next) => {
+    console.log('Middleware for /about - ' + Date.now());
+    next();
+});
+
+
+/************************************************/
+// Routes
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-
+ 
 app.get('/home', (req, res) => {
     res.send('Home Page');
 });
@@ -18,10 +48,12 @@ app.get('/books/:bookId', (req, res) => {
     res.send(req.params);
 });
 
-
-// For invalid routes
+/************************************************/
+// Final Invalid Route
 app.get('*', (req, res) => {
     res.send('404! This is an invalid URL.');
 });
 
+/************************************************/
+// Listener
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
